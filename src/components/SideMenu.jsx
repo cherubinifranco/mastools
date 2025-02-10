151be3447278779f1e75cba6ffd0429055579994
Cherubini Franco <cherubini.franco@hotmail.com>
2025-02-10 16:08:41 -0300

    fix: Margin top with new navigation

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -5,7 +5,7 @@ export default function SideMenu() {
   return (
     <div
       id="drawer-navigation"
-      className="fixed top-6 left-0 z-40 w-64 h-screen p-4 overflow-y-hidden -translate-x-[220px] focus-within:translate-x-0 group-active:translate-x-0 transition-transform hover:translate-x-0 bg-sidemenu hover:overflow-y-auto border-r-0"
+      className="fixed top-0 left-0 z-40 w-64 h-screen p-4 overflow-y-hidden -translate-x-[220px] focus-within:translate-x-0 group-active:translate-x-0 transition-transform hover:translate-x-0 bg-sidemenu hover:overflow-y-auto border-r-0"
       tabIndex="-1"
       aria-labelledby="drawer-navigation-label"
     >
